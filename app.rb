7fde3e8a0de2310ae69beeedc907edd9b1f49b15
em-ak <emilien.ak@gmail.com>
2013-10-03 12:38:10 +0200

    Create the navigation architecture

--- app.rb
+++ app.rb
@@ -5,3 +5,11 @@ require 'haml'
 get '/' do
   haml :index, :format => :html5
 end
+
+get '/:page' do
+  if File.exists?('views/'+params[:page]+'.haml')
+    haml params[:page].to_sym
+  else
+    raise error(404) 
+  end   
+end
